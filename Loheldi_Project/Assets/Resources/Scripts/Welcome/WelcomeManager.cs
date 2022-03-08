2f7804611a0df30c3d4d2343211fbec80efaeae1
MinJin <3tpepperok@gmail.com>
2022-03-08 16:00:35 +0900

    아이디/비밀번호 찾기단 구현

--- Loheldi_Project/Assets/Resources/Scripts/Welcome/WelcomeManager.cs
+++ Loheldi_Project/Assets/Resources/Scripts/Welcome/WelcomeManager.cs
@@ -16,11 +16,19 @@ public class WelcomeManager : MonoBehaviour
     GameObject SignupPanel;
     [SerializeField]
     GameObject SignupSucPanel;
+    [SerializeField]
+    GameObject FindAccPanel;
+    [SerializeField]
+    GameObject FindID;
+    [SerializeField]
+    GameObject FindPW;
 
-    private bool isLogin = false;
+    private bool isLogin = false;   //로그인 여부
     private bool isLPopup = false;  // 로그인 패널 활성화 여부
     private bool isSPopup = false;  // 회원가입 패널 활성화 여부
     private bool isSSPopup = false;  // 회원가입 성공 문구 패널 활성화 여부
+    private bool isAccPopup = false;  // 아이디/비밀번호 찾기 패널 활성화 여부
+    
 
     void Start()
     {
@@ -29,6 +37,7 @@ public class WelcomeManager : MonoBehaviour
         LoginPanel.SetActive(false);
         SignupPanel.SetActive(false);
         SignupSucPanel.SetActive(false);
+        FindAccPanel.SetActive(false);
 
     }
 
@@ -80,6 +89,28 @@ public class WelcomeManager : MonoBehaviour
             SignupSucPanel.SetActive(isSSPopup);
             Register.allOK = false;
         }
+    }
+
+    //아이디/비밀번호 찾기 패널 popup 관리
+    public void FindAccPopup()
+    {
+        isAccPopup = !isAccPopup;
+        FindAccPanel.SetActive(isAccPopup);
+        FindID.SetActive(true);
+        FindPW.SetActive(false);
+    }
+
+    //아이디 초기화 화면
+    public void FindIDPop()
+    {
+        FindID.SetActive(true);
+        FindPW.SetActive(false);
+    }
+    //비밀번호 초기화 화면
+    public void FindPWPop()
+    {
+        FindID.SetActive(false);
+        FindPW.SetActive(true);
     }
 
     public void Restart()   //회원가입 완료 후 재시작

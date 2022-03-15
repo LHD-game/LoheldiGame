94c2eff3b291c7ed2ad59508dc34a9e75492df7d
MinJin <3tpepperok@gmail.com>
2022-03-15 18:59:32 +0900

    계정 정보 서버에 저장 구현 성공!!

---
+++ Loheldi_Project/Assets/Resources/Scripts/DataClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//정보 저장 클래스
+
+
+//계정 정보
+public class AccInfo
+{
+    public string NICKNAME;
+    public DateTime BIRTH;
+}

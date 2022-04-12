45e4c87a028abd71c623e3e27a43bb495b610dc8
LeeKiHyuk-0404 <80307887+LeeKiHyuk-0404@users.noreply.github.com>
2022-04-12 15:21:46 +0900

    2022-0412 우편함 시작

---
+++ Loheldi_Project/Assets/Script/ReceivePost.cs
@@ -0,0 +1,47 @@
+using BackEnd;
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceivePost : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public class UPostItem
+    {
+        public string title;
+        public string author;
+        public string content;
+    }
+    public void ClickMail()
+    {
+        BackendReturnObject bro = Backend.UPost.GetPostList(PostType.Admin);
+        JsonData json = bro.GetReturnValuetoJSON()["postList"];
+
+
+        List<UPostItem> postItemList = new List<UPostItem>();
+        for(int i = 0; i < json.Count; i++)
+        {
+
+            UPostItem postItem = new UPostItem();
+            postItem.content = json[i]["content"].ToString();
+            postItem.title = json[i]["title"].ToString();
+
+            postItemList.Add(postItem);
+            Debug.Log(postItem);
+        }
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

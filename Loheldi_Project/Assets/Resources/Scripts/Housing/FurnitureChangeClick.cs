b8d856cae22b6d61a6d1db0e17fa4225b41eb7f7
Suyong <atz00j@naver.com>
2022-04-19 15:45:27 +0900

    하우징화면 잠깐 점검

--- Loheldi_Project/Assets/Resources/Scripts/Housing/FurnitureChangeClick.cs
+++ Loheldi_Project/Assets/Resources/Scripts/Housing/FurnitureChangeClick.cs
@@ -15,26 +15,26 @@ public class FurnitureChangeClick : MonoBehaviour
     {
         if (Input.GetMouseButtonUp(0))
         {
-            Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            Ray ray = getCamera.ScreenPointToRay(Input.mousePosition);         //마우스 위치에 RRayCast사용하기
+            if (Physics.Raycast(ray, out hit))                                      //만약 무언가를 눌랐다면
             {
-                if (CurrentFurniture != "Button")
+                if (CurrentFurniture != "Button")                                       //버튼을 누른게 아니라면
                 {
-                    Buttons.SetActive(false);
+                    Buttons.SetActive(false);                                               //버튼 치우기
                     ButtonToggle = false;
                     CurrentFurniture = "None";
                 }
 
-                if (hit.collider.gameObject.tag == "ChangeableFurniture")
+                if (hit.collider.gameObject.tag == "ChangeableFurniture")           //가구를 누른거라면
                 {
-                    if (CurrentFurniture == "None")
+                    if (CurrentFurniture == "None")                                     //그때 선택된 가구가 없다면
                     {
                         Debug.Log(hit.collider.gameObject.name);
-                        CurrentFurniture = hit.collider.gameObject.name;
+                        CurrentFurniture = hit.collider.gameObject.name;                      //해당 가구 선택하기
                     }
-                    if (!ButtonToggle)
+                    if (!ButtonToggle)                                                  //이때 버튼이 없다면
                     {
-                        Buttons.transform.position = Input.mousePosition;
+                        Buttons.transform.position = Input.mousePosition;                   //버튼을 클릭한 위치에 생성하기
                         Buttons.SetActive(true);
                         ButtonToggle = true;
                     }

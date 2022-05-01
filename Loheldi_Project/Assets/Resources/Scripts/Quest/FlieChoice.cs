07c3db8b84943f1e29a64244cf55b243509f9a2a
shine515 <70735607+shine515@users.noreply.github.com>
2022-05-01 15:44:36 +0900

    'f'

--- Loheldi_Project/Assets/Resources/Scripts/Quest/FlieChoice.cs
+++ Loheldi_Project/Assets/Resources/Scripts/Quest/FlieChoice.cs
@@ -11,6 +11,16 @@ public class FlieChoice : MonoBehaviour
     {
         chat = GameObject.Find("chatManager").GetComponent<LodingTxt>();
 
+        chat.Num = "1";
+        chat.FileAdress = "Scripts/Quest/Dialog";
+        chat.NewChat();
+    }
+
+    public void test2()
+    {
+        chat = GameObject.Find("chatManager").GetComponent<LodingTxt>();
+
+        chat.Num = "2";
         chat.FileAdress = "Scripts/Quest/Dialog";
         chat.NewChat();
     }

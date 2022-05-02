96197c0b0615d9396f6306ed37954ec69acee3ad
shine515 <70735607+shine515@users.noreply.github.com>
2022-05-02 13:00:14 +0900

    'd'

--- Loheldi_Project/Assets/Resources/Scripts/MainField/Interaction.cs
+++ Loheldi_Project/Assets/Resources/Scripts/MainField/Interaction.cs
@@ -7,13 +7,38 @@ public class Interaction : MonoBehaviour
 {
     public Text text;                               //점프버튼에 글자를 선언
     public bool NearNPC = false;                    //NPC근처에 있는지 확인하는 함수 선언
+    public string NameNPC;
+
+
+    /*private void OnControllerColloderHit(ControllerColliderHit hit) 
+    {
+        if (hit.collider.gameObject.name == "Nari")
+        {
+            Debug.Log("나리");
+        }
+
+        if (hit.collider.gameObject.name == "Nari")
+        {
+            Debug.Log("나리");
+        }
+    }*/
 
     void OnTriggerEnter(Collider other)             //다른 콜리더와 부딛혔을때
     {
         if (other.gameObject.tag == "NPC")          //콜리더의 Tag가 NPC라면
         {
             NearNPC = true;
-            text.text = "대화";
+            //text.text = "대화";
+            Debug.Log("대화");
+
+            if (other.gameObject.name == "Nari")
+            {
+                Debug.Log("나리");
+            }
+            else if (other.gameObject.name == "Nani")
+            {
+                Debug.Log("나니");
+            }
         }
     }
 
@@ -27,7 +52,8 @@ public class Interaction : MonoBehaviour
         if (other.gameObject.tag == "NPC")          //콜리더의 Tag가 NPC라면
         {
             NearNPC = false;
-            text.text = "점프";
+            //text.text = "점프";
+            Debug.Log("점프");
         }
     }
 }

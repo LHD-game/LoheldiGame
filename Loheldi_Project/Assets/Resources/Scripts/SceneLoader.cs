3ad582ca580f19a90c353d4cffd60599c9d02afe
shine515 <70735607+shine515@users.noreply.github.com>
2022-05-03 16:40:11 +0900

    'f'

--- Loheldi_Project/Assets/Resources/Scripts/SceneLoader.cs
+++ Loheldi_Project/Assets/Resources/Scripts/SceneLoader.cs
@@ -24,7 +24,7 @@ public class SceneLoader : MonoBehaviour
     }
     public void GotoGameMove()
     {
-        SceneManager.LoadScene("GameMove");
+        SceneManager.LoadScene("MainField");
     }
     public void GotoCreateAcc()
     {

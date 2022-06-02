a261f6bcfe3dc82e45c64b128f0d3721b7550453
shine515 <70735607+shine515@users.noreply.github.com>
2022-06-02 16:50:30 +0900

    '220602'

---
+++ Loheldi_Project/Assets/Resources/Scripts/Quest/VideoScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour
+{
+    public GameObject myVideo;
+    public VideoPlayer videoClip;
+
+    public void OnPlayVideo()
+    {
+        myVideo.SetActive(true);
+        videoClip.Play();
+    }
+
+    public void OnPauseVideo()
+    {
+        myVideo.SetActive(false);
+        videoClip.Pause();
+    }
+
+    public void OnResetVideo()
+    {
+        myVideo.SetActive(false);
+        videoClip.time = 0f;
+        videoClip.playbackSpeed = 1f;
+    }
+
+}

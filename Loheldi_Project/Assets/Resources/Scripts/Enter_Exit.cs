131b6daf79a3d1f8e45959bb4c0cde839b776a20
Suyong <atz00j@naver.com>
2022-07-19 15:04:20 +0900

    양치게임하트다썻을떄나가는버튼누르면소리가안나고오류가나면서게임먼춘걸대충고침

--- Loheldi_Project/Assets/Resources/Scripts/Enter_Exit.cs
+++ Loheldi_Project/Assets/Resources/Scripts/Enter_Exit.cs
@@ -6,11 +6,6 @@ public class Enter_Exit : MonoBehaviour //패널 열기/닫기에 사용되는 클래스
 {
     public GameObject SoundEffectManager;
 
-    private void Start()
-    {
-        SoundEffectManager = this.gameObject;
-    }
-
     public void ExitBtn(GameObject exitThis)    //해당 오브젝트를 비활성화
     {
         GameObject SoundManager = GameObject.Find("SoundManager");

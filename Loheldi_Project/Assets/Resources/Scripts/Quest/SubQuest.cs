8cb2a8037ec25336e34de9139fbccd8e7246049c
shine515 <70735607+shine515@users.noreply.github.com>
2022-11-08 17:35:03 +0900

    1108

--- Loheldi_Project/Assets/Resources/Scripts/Quest/SubQuest.cs
+++ Loheldi_Project/Assets/Resources/Scripts/Quest/SubQuest.cs
@@ -8,9 +8,15 @@ public class SubQuest : MonoBehaviour
 {
     public GameObject Main_UI;
     public GameObject AppleTree;
+    public GameObject AppleTreeOBJ;
     public TMP_InputField AppleTreeTxt;
     public GameObject ErrorWin;
     public Text ErrorWinTxt;
+    public MainGameManager MainUI;
+
+    [SerializeField]
+    private ParticleSystem HeartFx;
+
     public void AppleTreeQ()
     {
         if(AppleTreeTxt.text.Length<10)
@@ -28,8 +34,19 @@ public class SubQuest : MonoBehaviour
     {
         PlayInfoManager.GetExp(10);
         PlayInfoManager.GetCoin(10);
+        MainUI.UpdateField();
         AppleTree.SetActive(false);
         Main_UI.SetActive(true);
+        HeartFX(AppleTreeOBJ);
+    }
+
+    public void HeartFX(GameObject go)    //머리 반짝!하는 파티클
+    {
+        Debug.Log("반짝");
+        ParticleSystem newfx = Instantiate(HeartFx);
+        newfx.transform.position = go.transform.position + new Vector3(0, 7, -3);
+
+        newfx.Play();
     }
     // Start is called before the first frame update
     /*void Start()
